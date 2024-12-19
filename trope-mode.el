c54139fe108929337289126fd4e8a2380e421703
Sean Vo <101959987+TriAttack238@users.noreply.github.com>
2024-12-18 22:16:28 -0800

    Create trope-mode.el to put package code

---
+++ trope-mode.el
@@ -0,0 +1,36 @@
+;;; trope-mode.el --- Major mode to edit TV Tropes format ".trp" files -*- lexical-binding:t -*-
+
+;; Copyright (C) 2024 Sean Vo
+;; Author: Sean Vo <triattack238@gmal.com>
+;; Version: 0.0.1
+;; Created: 18 Dec 2024
+;; package-requires ((emacs "29.1"))
+;; Keywords: TV Tropes, trope
+;; URL: https://github.com/TriAttack238/trope-mode
+
+;; This file is not a part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See the README.org file for details.
+
+;;; Code:
+
+
+(provide 'trope-mode)
+
+;;; trope-mode.el ends here
+
